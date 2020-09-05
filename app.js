342a52ac6ef7bac316b1a1e4ab1114cf3a4456cd
Rohit <rohitkumar377024@gmail.com>
2020-09-06 05:06:34 +0530

    Added: Mongoose and MongoDB Connection

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const run = async () => {
+  await mongoose.connect(
+    'mongodb+srv://admin-rohit:test123@cluster0-exv7e.mongodb.net/furniture-store',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
   app.listen(3000, () => console.log('Listening on Port 3000'));
 };
 
