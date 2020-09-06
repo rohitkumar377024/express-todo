9f5eef89a304900365a0f67ec8276eff0ad0f4cb
Rohit <rohitkumar377024@gmail.com>
2020-09-06 05:33:15 +0530

    Added: Add Todo Controller

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const todoRouter = require('./routes/todo');
-app.use('/todo', todoRouter);
+app.use('/todos', todoRouter);
 
 const run = async () => {
   await mongoose.connect(
-    'mongodb+srv://admin-rohit:test123@cluster0-exv7e.mongodb.net/furniture-store',
+    'mongodb+srv://admin-rohit:test123@cluster0-exv7e.mongodb.net/todo',
     { useNewUrlParser: true, useUnifiedTopology: true }
   );
   app.listen(3000, () => console.log('Listening on Port 3000'));
--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,14 @@
 const Todo = require('../models/todo');
 
-const addTodo = async (req, res) => {};
+const addTodo = async (req, res) => {
+  try {
+    await new Todo(req.body).save();
+    res.json({ status: 'error', message: 'Successfully added todo.' });
+  } catch (e) {
+    res.json({ status: 'error', message: 'Error adding todo.' });
+  }
+};
+
 const findAllTodos = async (req, res) => {};
 const updateTodo = async (req, res) => {};
 const deleteTodo = async (req, res) => {};
