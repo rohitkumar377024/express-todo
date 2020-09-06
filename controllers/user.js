462a6612c26564a76ce97232fdd81922f6533c57
Rohit <rohitkumar377024@gmail.com>
2020-09-06 05:35:03 +0530

    Added: Find All Todos Controller

--- controllers/user.js
+++ controllers/user.js
@@ -3,13 +3,21 @@ const Todo = require('../models/todo');
 const addTodo = async (req, res) => {
   try {
     await new Todo(req.body).save();
-    res.json({ status: 'error', message: 'Successfully added todo.' });
+    res.json({ status: 'success', message: 'Successfully added todo.' });
   } catch (e) {
     res.json({ status: 'error', message: 'Error adding todo.' });
   }
 };
 
-const findAllTodos = async (req, res) => {};
+const findAllTodos = async (req, res) => {
+  try {
+    const allTodos = await Todo.find();
+    res.json({ status: 'success', message: allTodos });
+  } catch (e) {
+    res.json({ status: 'error', message: 'Could not fetch todos.' });
+  }
+};
+
 const updateTodo = async (req, res) => {};
 const deleteTodo = async (req, res) => {};
 
